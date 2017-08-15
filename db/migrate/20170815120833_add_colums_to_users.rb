c62748c1886662bb111e50ad86740e26955e53d9
Richard Eskut <richard.eskut@gmail.com>
2017-08-15 14:32:28 +0000

    scaffold crud articles

---
+++ db/migrate/20170815120833_add_colums_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean, default: false
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+  end
+end
