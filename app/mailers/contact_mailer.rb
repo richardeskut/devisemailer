a8eddfb2a73dcfe38670b1016557806f9aeb54bd
Richard Eskut <richard.eskut@gmail.com>
2017-08-16 10:43:24 +0000

    contact

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+  default to: "steve@chesnowitz.com"
+  
+  def contact_email(name, email, message)
+    @name = name
+    @email = email
+    @message = message
+
+  
+  mail(from: email, subject: "Richards Articles Contact Form Message")
+  end
+end
